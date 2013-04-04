c2d59b05b3f6553c0b4d17ea3824083ab6cf835b
zeroshift <zeroshiftsl@gmail.com>
2013-04-03 21:15:45 -0500

    Updated setup.

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import sys
 import os
 from distutils.core import setup
-
-import zeroshift_hue
+#from setuptools import setup, find_packages
 
 setup(name='zeroshift_hue',
-      version="0.1",
-      description='Module for Philips Hue bulbs.',
-      long_description="Module for Philips Hue bulbs.",
-      author="Nick Prendergast",
-      author_email='neprendergast@gmail.com',
-      url='https://github.com/zeroshift/zeroshift_hue',
-      py_modules=['zeroshift_hue'],
-      scripts=['zeroshift_hue.py'],
-      license='BSD'
+    version="0.1",
+    description='Module for Philips Hue bulbs.',
+    long_description="Module for Philips Hue bulbs.",
+    author="Nick Prendergast",
+    author_email='neprendergast@gmail.com',
+    url='https://github.com/zeroshift/zeroshift_hue',
+    py_modules=['zeroshift_hue'],
+    scripts = ['zeroshift_hue.py'],
+    #packages = find_packages(),
+    license='MIT'
 )
