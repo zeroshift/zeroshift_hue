eda4e6d6878af00a4a1d6c6fcc7617d230c6a54e
zeroshift <zeroshiftsl@gmail.com>
2013-06-24 22:35:18 -0500

    Made some minor code changes. Better for ipython, and use logging instead of printing.

--- zeroshift_hue.py
+++ zeroshift_hue.py
@@ -4,25 +4,16 @@ import json
 import requests
 import sys, time
 from threading import *
+import logging
 
 class Hue(object):
-    def __init__(self, devicetype="zeroshift-hue", username=None, bridge_ip=None):
+    def __init__(self, devicetype="zeroshift-hue", username=None, bridge_ip=None, debug=False):
         self.bridge_ip = bridge_ip
         if not self.bridge_ip:
-          self.bridge_ip = self.getBridgeIP() 
+          self.bridge_ip = self._getBridgeIP() 
         
         self.devicetype = devicetype
         self.username = username
-
-    def getBridgeIP(self): 
-        try:
-            response = self.discoverLocalBridges()
-            bridge_ip = response[0]['internalipaddress']
-            print "Discovered bridge. IP: %s" % bridge_ip
-            return bridge_ip
-        except:
-            print "Could not get bridge IP!"
-            return False
 
     def authenticate(self):
         did_get_username = False
@@ -31,23 +22,28 @@ class Hue(object):
         else:
             count = 0
             while count < 6:
-                response = self.createUser(self.devicetype)
-                # Note: Fix this. It's ugly
-                try:
-                    if response[0]['error']:
-                        print "Warning: %" % response[0]['error']['description']
-                except:
-                    pass
-                try:
-                    if response[0]['success']:
-                        self.username = response[0]['success']['username']
-                        did_get_username = True
-                        break
-                except:
-                    pass
+                response = self._createUser(self.devicetype)
+                if response[0].has_key('error'):
+                    logging.warning("Warning: %" % response[0]['error']['description'])
+                elif response[0].has_key('success'):
+                    self.username = response[0]['success']['username']
+                    did_get_username = True
+                    break
+                else:
+                    logging.error('Something went wrong.')
                 count += 1
                 time.sleep(5)
             return did_get_username
+
+    def _getBridgeIP(self): 
+        try:
+            response = self._discoverLocalBridges()
+            bridge_ip = response[0]['internalipaddress']
+            logging.debug("Discovered bridge. IP: %s" % bridge_ip)
+            return bridge_ip
+        except:
+            logging.debug("Could not get bridge IP!")
+            return False
 
     # Custom
     def getLightObject(self, light_id):
@@ -56,42 +52,42 @@ class Hue(object):
 
     def getAllLightObjects(self):
         lights = []
-        for light_id in self.getAllLights():
+        for light_id in self._getAllLights():
             light = Light(self, light_id)
             lights.append(light)
         return lights
 
-    def getLightState(self, light_id):
+    def _getLightState(self, light_id):
         r = requests.get(("http://%s/api/%s/lights/%s" % (self.bridge_ip, self.username, light_id)))
         return json.loads(r.text)['state']
 
-    def setLightStateWithPayload(self, light_id, payload):
+    def _setLightStateWithPayload(self, light_id, payload):
         r = requests.put(("http://%s/api/%s/lights/%s/state" % (self.bridge_ip, self.username, light_id)), json.dumps(payload))
         return json.loads(r.text)
 
     # Lights
-    def getAllLights(self):
+    def _getAllLights(self):
         r = requests.get(("http://%s/api/%s/lights" % (self.bridge_ip, self.username)))
         return json.loads(r.text)
 
-    def getNewLights(self):
+    def _getNewLights(self):
         r = requests.get(("http://%s/api/%s/lights/new" % (self.bridge_ip, self.username)))
         return json.loads(r.text)
 
-    def searchNewLights(self):
+    def _searchNewLights(self):
         r = requests.post(("http://%s/api/%s/lights" % (self.bridge_ip, self.username)))
         return json.loads(r.text)
 
-    def getLightAttribsAndState(self, light_id):
+    def _getLightAttribsAndState(self, light_id):
         r = requests.get(("http://%s/api/%s/lights/%s" % (self.bridge_ip, self.username, light_id)))
         return json.loads(r.text)
 
-    def setLightName(self, light_id, name):
+    def _setLightName(self, light_id, name):
         payload = {'name': name}
         r = requests.put(("http://%s/api/%s/lights/%s" % (self.bridge_ip, self.username, light_id)), json.dumps(payload))
         return json.loads(r.text)
 
-    def setLightState(self, light_id, **kwargs):
+    def _setLightState(self, light_id, **kwargs):
         """ Required: light_id. Optional: on, bri, hue, sat, xy, ct, alert, effect, transiontime. """
         payload = {}
         for key,value in kwargs.items():
@@ -100,18 +96,18 @@ class Hue(object):
         return json.loads(r.text)   
     
     ## Groups
-    def getAllGroups(self):
+    def _getAllGroups(self):
         r = requests.get(("http://%s/api/%s/groups" % (self.bridge_ip, self.username)))
         return json.loads(r.text)
 
-    def createGroup(self, ):
-        print "Not yet implemented." 
+    def _createGroup(self, ):
+        logging.info("Not yet implemented.")
         
-    def getGroupAttribs(self, group_id):
+    def _getGroupAttribs(self, group_id):
         r = requests.get(("http://%s/api/%s/groups/%s" % (self.bridge_ip, self.username, group_id)))
         return json.loads(r.text)
 
-    def setGroupAttribs(self, group_id, **kwargs):
+    def _setGroupAttribs(self, group_id, **kwargs):
         """ Required: group_id. Optional: name, lights"""
         payload = {}
         for key,value in kwargs.items():
@@ -119,7 +115,7 @@ class Hue(object):
         r = requests.put(("http://%s/api/%s/groups/%s" % (self.bridge_ip, self.username, group_id)), json.dumps(payload))
         return json.loads(r.text)
 
-    def setGroupState(self, group_id, **kwargs):
+    def _setGroupState(self, group_id, **kwargs):
         """ Required group_id. Optional: on, bri, hue, sat, xy, ct, alert, effect, transiontime. """
         payload = {}
         for key,value in kwargs.items():
@@ -127,15 +123,15 @@ class Hue(object):
         r = requests.put(("http://%s/api/%s/groups/%s/action" % (self.bridge_ip, self.username, group_id)), json.dumps(payload))
         return json.loads(r.text)
 
-    def deleteGroup(self):
-        print "Not yet implemented." 
+    def _deleteGroup(self):
+        logging.info("Not yet implemented.")
 
     ## Schedules
-    def getAllSchedules(self):
+    def _getAllSchedules(self):
         r = requests.get(("http://%s/api/%s/schedules" % (self.bridge_ip, self.username)))
         return json.loads(r.text)
 
-    def createSchedule(self, command, time, **kwargs):
+    def _createSchedule(self, command, time, **kwargs):
         """ Required: command, time. Optional: name, description. """
         payload = {'command': command, 'time': time}
         for key,value in kwargs.items():
@@ -143,11 +139,11 @@ class Hue(object):
         r = requests.post(("http://%s/api/%s/schedules" % (self.bridge_ip, self.username)), json.dumps(payload))
         return json.loads(r.text)
 
-    def getScheduleAttribs(self, schedule_id):
+    def _getScheduleAttribs(self, schedule_id):
         r = requests.get(("http://%s/api/%s/schedules/%s" % (self.bridge_ip, self.username, schedule_id)))
         return json.loads(r.text)
 
-    def setScheduleAttribs(self, schedule_id, **kwargs):
+    def _setScheduleAttribs(self, schedule_id, **kwargs):
         """ Required: schedule_id. Optional: name, description, command, time. """
         payload = {}
         for key,value in kwargs.items():
@@ -155,12 +151,12 @@ class Hue(object):
         r = requests.put(("http://%s/api/%s/schedules/%s" % (self.bridge_ip, self.username, schedule_id)), json.dumps(payload))
         return json.loads(r.text)
 
-    def deleteSchedule(self, schedule_id):
+    def _deleteSchedule(self, schedule_id):
         r = requests.delete(("http://%s/api/%s/schedules/%s" % (self.bridge_ip, self.username, schedule_id)))
         return json.loads(r.text)
 
     ## Config
-    def createUser(self, devicetype, username=None):
+    def _createUser(self, devicetype, username=None):
         payload = {'devicetype': devicetype}
         if username:
             payload['username'] = username
@@ -168,11 +164,11 @@ class Hue(object):
         r = requests.post(("http://%s/api" % (self.bridge_ip)), json.dumps(payload))
         return json.loads(r.text)
 
-    def getConfig(self):
+    def _getConfig(self):
         r = requests.get(("http://%s/api/%s/config" % (self.bridge_ip, self.username)))
         return json.loads(r.text)
 
-    def modifyConfig(self, **kwargs):
+    def _modifyConfig(self, **kwargs):
         """ Optional: proxyport, name, swupdate, proxyaddress, linkbutton, ipaddress, netmask, gateway, dhcp, portalservices. """
         payload = {}
         for key,value in kwargs.items():
@@ -180,16 +176,16 @@ class Hue(object):
         r = requests.put(("http://%s/api/%s/config" % (self.bridge_ip, self.username, schedule_id)), json.dumps(payload))
         return json.loads(r.text)
 
-    def deleteUserFromWhitelist(self, username):
+    def _deleteUserFromWhitelist(self, username):
         r = requests.delete(("http://%s/api/%s/config/whitelist/%s" % (self.bridge_ip, self.username, username)))
         return json.loads(r.text)
 
-    def getFullState(self):
+    def _getFullState(self):
         r = requests.get(("http://%s/api/%s" % (self.bridge_ip, self.username)))
         return json.loads(r.text)
 
     # Portal API
-    def discoverLocalBridges(self):
+    def _discoverLocalBridges(self):
         r = requests.get("http://www.meethue.com/api/nupnp")
         return json.loads(r.text)
 
@@ -207,16 +203,16 @@ class Light(object):
         self.crelax  = 13068
     
     def on(self):
-        self.hue.setLightState(self.light_id, on=True)
+        self.hue._setLightState(self.light_id, on=True)
 
     def off(self):
-        self.hue.setLightState(self.light_id, on=False)
+        self.hue._setLightState(self.light_id, on=False)
     
     def relax(self):
-        self.hue.setLightState(self.light_id, on=True, hue=self.crelax, sat=200, bri=170)
+        self.hue._setLightState(self.light_id, on=True, hue=self.crelax, sat=200, bri=170)
 
     def alert(self, alert_type="select"):
-        self.setLightState(self.light_id, alert=alert_type)
+        self.hue._setLightState(self.light_id, alert=alert_type)
     
     def blinkPurple(self):
         self.blink(self.purple)
@@ -234,10 +230,10 @@ class Light(object):
         self.blink(self.red)
 
     def blink(self, color):
-        state = self.hue.getLightAttribsAndState(self.light_id)['state']
-        response = self.hue.setLightState(self.light_id, on=True, \
+        state = self.hue._getLightAttribsAndState(self.light_id)['state']
+        response = self.hue._setLightState(self.light_id, on=True, \
             hue=color, bri=255, sat=255, transiontime=10)
         time.sleep(1)
-        response = self.hue.setLightState(self.light_id, on=True, alert="select")
+        response = self.hue._setLightState(self.light_id, on=True, alert="select")
         time.sleep(1)
-        self.hue.setLightStateWithPayload(self.light_id, state)
+        self.hue._setLightStateWithPayload(self.light_id, state)
